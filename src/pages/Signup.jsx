1fd0e6fcf1f62a358f86a81f3bae9396915b7e26
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2022-11-08 13:26:40 +0100

    structure initial done

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+function Signup() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (e) => setEmail(e.target.value);
+  const handlePasswordChange = (e) => setPassword(e.target.value);
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+    // ... signup logic here
+  };
+
+  return (
+    <div>
+
+      <h1>Sign Up</h1>
+    
+      <form onSubmit={handleSignup}>
+
+        <label>Email:</label>
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={handleEmailChange}
+        />
+
+        <label>Password:</label>
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+
+        <button type="submit">Signup</button>
+      </form>
+      
+    </div>
+  );
+}
+
+export default Signup;
